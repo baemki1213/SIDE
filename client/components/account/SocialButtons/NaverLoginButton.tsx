7f9807753b04028bda95097283ddddab3197f046
baemki1213 <baemki1213@gmail.com>
2023-06-06 18:40:40 +0900

    Pass social btn test

--- client/components/account/SocialButtons/NaverLoginButton.tsx
+++ client/components/account/SocialButtons/NaverLoginButton.tsx
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 
 export default function NaverLoginButton() {
-  return <NaverLoginButtonContainer></NaverLoginButtonContainer>;
+  return <NaverLoginButtonContainer>naver</NaverLoginButtonContainer>;
 }
 
-const NaverLoginButtonContainer = styled.div`
+const NaverLoginButtonContainer = styled.button`
   width: 48px;
   aspect-ratio: 1;
   border-radius: 100%;
   background-color: greenyellow;
   margin: 0 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
