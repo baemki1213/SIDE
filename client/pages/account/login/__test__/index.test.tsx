9855d9b96af559455f22f38a129604839029cff6
baemki1213 <baemki1213@gmail.com>
2023-11-18 16:07:25 +0900

    Update login page test case

--- client/pages/account/login/__test__/index.test.tsx
+++ client/pages/account/login/__test__/index.test.tsx
@@ -3,9 +3,11 @@ import {
   mockConsoleError,
   render,
   screen,
+  waitFor,
 } from "@/utils/test-utils";
 import LoginPage from "..";
 import { colors } from "@/styles/assets";
+import { instance } from "@/api/Core";
 
 describe("Login page", () => {
   mockConsoleError();
@@ -37,10 +39,55 @@ describe("Login page", () => {
     fireEvent.change(emailInput, { target: { value: "email" } });
     expect(emailInput).toHaveStyle(`color: ${colors.redFF}`);
   });
-  test("Should clear password inputs after click signIn button", async () => {});
-  test("Should show success message on successful login", async () => {});
-  test("Should show error message on failed login", async () => {});
-  test("Should display error message on server error", async () => {});
-  test("Should disable login button during loading", async () => {});
-  test("Should clear inputs after successful login", async () => {});
+  test("Should clear password inputs after click signIn button", async () => {
+    const signInButton = screen.getByRole("button", { name: "로그인" });
+    fireEvent.click(signInButton);
+    const passwordInput = screen.getByPlaceholderText("비밀번호");
+    expect(passwordInput).toHaveValue("");
+  });
+  test("Should show success message on successful login", async () => {
+    const emailInput = screen.getByPlaceholderText("이메일");
+    fireEvent.change(emailInput, { target: { value: "valid@email.com" } });
+    const passwordInput = screen.getByPlaceholderText("비밀번호");
+    fireEvent.change(passwordInput, { target: { value: "1234qwer!@" } });
+    const signInButton = screen.getByRole("button", { name: "로그인" });
+    fireEvent.click(signInButton);
+    const successMessage = screen.getByText("안녕하세요!");
+    expect(successMessage).toBeInTheDocument();
+    await waitFor(() => {
+      const mainPageTitle = screen.getByText("main");
+      expect(mainPageTitle).toBeInTheDocument();
+    });
+  });
+  test("Should show error message on failed login", async () => {
+    const emailInput = screen.getByPlaceholderText("이메일");
+    fireEvent.change(emailInput, { target: { value: "invalid@email.com" } });
+    const passwordInput = screen.getByPlaceholderText("비밀번호");
+    fireEvent.change(passwordInput, { target: { value: "1234qwer!@" } });
+    const signInButton = screen.getByRole("button", { name: "로그인" });
+    fireEvent.click(signInButton);
+    const failMessage = screen.getByText(
+      "아이디 혹은 비밀번호를 확인해주세요."
+    );
+    expect(failMessage).toBeInTheDocument();
+  });
+  test("Should display error message on server error", async () => {
+    instance.defaults.headers.common["X-Use-Mock-Error"] = true;
+
+    const emailInput = screen.getByPlaceholderText("이메일");
+    fireEvent.change(emailInput, { target: { value: "valid@email.com" } });
+    const passwordInput = screen.getByPlaceholderText("비밀번호");
+    fireEvent.change(passwordInput, { target: { value: "1234qwer!@" } });
+    const signInButton = screen.getByRole("button", { name: "로그인" });
+    fireEvent.click(signInButton);
+    const serverErrorText = screen.getByText(
+      "서버 에러...! 잠시후 다시 시도해주세요."
+    );
+    expect(serverErrorText).toBeInTheDocument();
+  });
+  test("Should disable login button during loading", async () => {
+    const signInButton = screen.getByRole("button", { name: "로그인" });
+    fireEvent.click(signInButton);
+    expect(signInButton).toBeDisabled();
+  });
 });
