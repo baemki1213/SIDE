b043e2385a01493ef58748abb3342470ec538ec8
baemki1213 <baemki1213@gmail.com>
2023-11-29 00:03:15 +0900

    Update login form, login page

--- client/pages/account/login/index.tsx
+++ client/pages/account/login/index.tsx
@@ -1,12 +1,33 @@
-import * as S from "./styles";
+import { ChangeEvent, useState } from "react";
 
+import LoginForm from "@/components/account/login/LoginForm";
 import LoginTitle from "@/components/account/login/LoginTitle";
 
+import * as S from "./styles";
+
 export default function LoginPage() {
+  const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    e.preventDefault();
+    switch (e.target.name) {
+      default:
+        setLoginInfo(prev => ({ ...prev, [e.target.name]: e.target.value }));
+    }
+  };
   return (
     <S.Container>
       <S.Wrapper>
         <LoginTitle />
+        <LoginForm
+          handleChange={handleChange}
+          email={loginInfo.email}
+          password={loginInfo.password}
+        />
       </S.Wrapper>
     </S.Container>
   );
