0ce109a77cb58af7a6e74d03b2c2e39e2913297e
baemki1213 <baemki1213@gmail.com>
2023-12-04 23:34:50 +0900

    Update GridSystem

---
+++ client/components/common/Layout/GridSystem.tsx
@@ -0,0 +1,40 @@
+// TypeScript를 사용한 MUI 스타일의 그리드 시스템 구현
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+
+// 컨테이너 스타일 정의
+const GridContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+`;
+
+// 아이템 스타일 정의
+const StyledGridItem = styled.div<{ size: number }>`
+  padding-right: 15px;
+  padding-left: 15px;
+  flex: 0 0 ${props => (props.size / 12) * 100}%;
+  max-width: ${props => (props.size / 12) * 100}%;
+`;
+
+// 그리드 컨테이너 컴포넌트 Props 타입 정의
+interface GridProps {
+  children: ReactNode;
+}
+
+// 그리드 컨테이너 컴포넌트
+export const Grid: React.FC<GridProps> = ({ children }) => {
+  return <GridContainer>{children}</GridContainer>;
+};
+
+// 그리드 아이템 컴포넌트 Props 타입 정의
+interface GridItemProps {
+  size: number;
+  children: ReactNode;
+}
+
+// 그리드 아이템 컴포넌트
+export const GridItem: React.FC<GridItemProps> = ({ size, children }) => {
+  return <StyledGridItem size={size}>{children}</StyledGridItem>;
+};
