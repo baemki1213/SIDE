f87eaba369dd548602a8b086d8020fbf15244146
baemki1213 <baemki1213@gmail.com>
2023-12-07 00:03:20 +0900

    Fix authenticateAccessToken env

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -10,12 +10,13 @@ export const authenticateAccessToken = (
 ) => {
   const authHeader = req.headers.authorization;
   const accessToken = authHeader && authHeader.split(" ")[1];
+
   if (accessToken == null)
     return res.status(401).send("액세스 토큰이 존재하지 않습니다.");
 
   jwt.verify(
     accessToken,
-    process.env.ACCESS_TOKEN_SECRET,
+    process.env.JWT_ACCESS_SECRET,
     (err: any, user: any) => {
       if (err) return res.status(403).send("유효하지 않은 액세스 토큰"); // 유효하지 않은 토큰
       req.user = user;
