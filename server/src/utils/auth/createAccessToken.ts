9ae6ab285ad760c9b44b241a30c4a915f34541e4
baemki1213 <baemki1213@gmail.com>
2023-12-08 00:20:31 +0900

    change access_token expires 15m to 30m

--- server/src/utils/auth/createAccessToken.ts
+++ server/src/utils/auth/createAccessToken.ts
@@ -2,6 +2,6 @@ const jwt = require("jsonwebtoken");
 
 export const createAccessToken = async (userId: number) => {
   return jwt.sign({ id: userId }, process.env.JWT_ACCESS_SECRET, {
-    expiresIn: "15m",
+    expiresIn: "30m",
   });
 };
