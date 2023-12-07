8a7b63f88cb340957144f177cd920b294f9d9c36
baemki1213 <baemki1213@gmail.com>
2023-12-07 22:43:34 +0900

    delete log

--- client/pages/main.tsx
+++ client/pages/main.tsx
@@ -1,19 +1,3 @@
-import { useAppDispatch, useAppSelector } from "@/hooks/reduxHook";
-import { selectAuthState } from "@/store/authSlice";
-import { fetchUserInfoAndUpdateRedux } from "@/utils/functions/user/fetchUserInfoAndUpdateRedux";
-
 export default function Main() {
-  const dispatch = useAppDispatch();
-  const { access_token } = useAppSelector(selectAuthState);
-  console.log(access_token);
-  return (
-    <div>
-      main
-      <button
-        onClick={() => fetchUserInfoAndUpdateRedux({}, access_token, dispatch)}
-      >
-        get user info
-      </button>
-    </div>
-  );
+  return <div>main</div>;
 }
