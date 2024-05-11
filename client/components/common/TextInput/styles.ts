6474749dad6c045b9ff97c1341a0162584a56074
baemki1213 <baemki1213@gmail.com>
2024-05-11 17:19:29 +0900

    TextInput container width 100%

--- client/components/common/TextInput/styles.ts
+++ client/components/common/TextInput/styles.ts
@@ -4,6 +4,7 @@ import styled from "styled-components";
 export const TextInputContainer = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
 `;
 
 export const TextInputTextBox = styled.input<{
