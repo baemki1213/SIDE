edb3a6920dac4fc15adce51bd582792650b4db83
baemki1213 <baemki1213@gmail.com>
2024-05-11 23:38:34 +0900

    Fix create address string

--- server/src/utils/service/createAddressString.ts
+++ server/src/utils/service/createAddressString.ts
@@ -4,8 +4,9 @@ export function createAddressString(data: Data): string {
   const result = data.results[0];
   const addressParts: string[] = [];
 
-  for (let i = 1; i <= 4; i++) {
+  for (let i = 1; i <= 2; i++) {
     const areaName = result.region[`area${i}` as keyof Region].name;
+    console.log(areaName);
     if (areaName) {
       addressParts.push(areaName);
     }
