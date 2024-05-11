b6ce7da4f7ba5b5cfc8f71267ece30c054868105
baemki1213 <baemki1213@gmail.com>
2024-05-11 15:31:37 +0900

    Update fetchAddress api

--- client/api/map.ts
+++ client/api/map.ts
@@ -2,15 +2,16 @@ import { requestWithCookie } from "@/utils/Axios-utils";
 
 interface FetchAddressesParams {
   query: string;
-  latitude: number;
-  longitude: number;
+  latitude: number | null;
+  longitude: number | null;
   distance: number;
 }
 
 export const fetchAddresses = async (params: FetchAddressesParams) => {
   const { query, latitude, longitude, distance } = params;
+  if (!latitude || !longitude) return null;
   return await requestWithCookie({
     method: "get",
-    url: `/service/map/circle?latitude=${latitude}&longitude=${longitude}&distance=${distance}&query=${query}`,
+    url: `/service/map/circle?latitude=${latitude}&longitude=${longitude}&distance=${distance}&query=${query}&max_page=4`,
   });
 };
